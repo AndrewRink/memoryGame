68e2582ce44a21cad4c31a598db200ec9cc0346b
Andrew Rink <Andrewcrink@gmail.com>
2024-01-08 11:45:18 -0500

    card array created and shuffling successfully

---
+++ app.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+        name: "fries",
+        img: "images/fries.jpg"
+    },
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.jpg"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.jpg"
+    },
+    {
+        name: "ice-cream",
+        img: "images/icecream.jpg"
+    },
+    {
+        name: "milkshake",
+        img: "images/milkshake.jpg"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.jpg"
+    },
+    {
+        name: "fries",
+        img: "images/fries.jpg"
+    },
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.jpg"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.jpg"
+    },
+    {
+        name: "ice-cream",
+        img: "images/icecream.jpg"
+    },
+    {
+        name: "milkshake",
+        img: "images/milkshake.jpg"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.jpg"
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
